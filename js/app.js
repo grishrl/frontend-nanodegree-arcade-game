8445313add0352da42f7a18f318514d473c1d693
grishrl <lee_grisham05@yahoo.com>
2015-02-11 22:47:13 -0600

    Generate and move player, generate and move enemy.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(b) {
+     this.x = 0;
+     this.y = b*63;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -7,29 +9,84 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 }
-
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x+150*dt;
 }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+     console.log("enemy "+ this.x + " " + this.y);
+    ctx.drawImage(Resources.get(this.sprite),this.x, this.y);
 }
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var carl = new Enemy(1);
+var bob = new Enemy(2);
+var jones = new Enemy(3);
+var rhonda = new Enemy(1);
+var allEnemies=[];
+allEnemies.push(carl);
+allEnemies.push(bob);
+allEnemies.push(jones);
+allEnemies.push(rhonda);
 
+var player = {
+    "x":2,
+    "y":5,
+    "update":function(){        
+    },
+    "render":function(){
+        console.log("player "+this.x + " " + this.y);
+        if(this.x>=0&&this.x<=4)
+        {
+            if(this.y>0&&this.y<=5)
+            {
+             ctx.drawImage(Resources.get("images/char-boy.png"), this.x*101, this.y*73);
+            }else{
+               if(this.y>5)
+               this.y--;
+               if(this.y<1)
+               this.y++; 
+                ctx.drawImage(Resources.get("images/char-boy.png"), this.x*101, this.y*73);
+            }
+        }else{
+            if(this.x>4)
+               this.x--;
+               if(this.x<0)
+               this.x++; 
+                ctx.drawImage(Resources.get("images/char-boy.png"), this.x*101, this.y*73);
+        }
+        
+    },
+    "handleInput":function(key){
+        switch(key)
+        {
+            case 'left':
+            this.x=this.x-1;
+            break;
+            case 'up':
+            this.y=this.y-1;
+            break;
+            case 'right':
+            this.x=this.x+1;
+            break;
+            case 'down':
+            this.y=this.y+1;
+            break;
+        }
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
 
 
 // This listens for key presses and sends the keys to your
